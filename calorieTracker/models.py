bf01de8c9ee7849ecaa1b470ca19d63a49f8ea79
SilviaMaina <sylviahmaina6@gmail.com>
2025-05-11 16:01:02 +0300

    create model

--- calorieTracker/models.py
+++ calorieTracker/models.py
@@ -1,9 +1,9 @@
-# from django.db import models
+from django.db import models
 
-# class FoodItem(models.Model):
-#     name = models.CharField(max_length=100)
-#     calories = models.PositiveIntegerField()
-#     date_added = models.DateField(auto_now_add=True)
+class FoodItem(models.Model):
+    name = models.CharField(max_length=100)
+    calories = models.PositiveIntegerField()
+    date_added = models.DateField(auto_now_add=True)
 
-#     def __str__(self):
-#         return f"{self.name} - {self.calories} cal"
+    def __str__(self):
+        return f"{self.name} - {self.calories} cal"
